fbe6c04fcb00427558975a54f76ea0113ff267ae
Arnaldo Junior <abs.junnior@hotmail.com>
2024-05-01 19:02:02 -0300

    Criação da model AuthFormData -  autenticação #2

---
+++ lib/models/auth_form_data.dart
@@ -0,0 +1,23 @@
+import 'dart:io';
+
+enum AuthMode { login, signup }
+
+class AuthFormData {
+  String nome = "";
+  String email = "";
+  String password = "";
+  File? image;
+  AuthMode _mode = AuthMode.login;
+
+  bool get isLogin {
+    return _mode == AuthMode.login;
+  }
+
+  bool get isSignup {
+    return _mode == AuthMode.signup;
+  }
+
+  void toggleAuthMode() {
+    _mode = isLogin ? AuthMode.signup : AuthMode.login;
+  }
+}

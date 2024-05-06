72a5eca3336414d2ed7574d9d304c8d74018e85c
Arnaldo Junior <abs.junnior@hotmail.com>
2024-05-05 22:45:47 -0300

    Personalização das informações da conversa

--- lib/components/message_bubble.dart
+++ lib/components/message_bubble.dart
@@ -1,53 +1,94 @@
+import 'dart:io';
 import 'package:chate_ease/core/models/chat_message.dart';
 import 'package:flutter/material.dart';
 
 class MessageBubble extends StatelessWidget {
+  static const _defualtImage = 'assets/image/avatar.png';
   final ChatMessage message;
   final bool belongsToCurretuser;
+
   const MessageBubble({
     super.key,
     required this.message,
     required this.belongsToCurretuser,
   });
 
+  Widget _showUserImage(String imageUrl) {
+    ImageProvider? provider;
+    final uri = Uri.parse(imageUrl);
+
+    if (uri.path.contains(_defualtImage)) {
+      provider = const AssetImage(_defualtImage);
+    } else if (uri.scheme.contains('http')) {
+      provider = NetworkImage(uri.toString());
+    } else {
+      provider = FileImage(File(uri.toString()));
+    }
+
+    return CircleAvatar(
+      backgroundImage: provider,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size.width;
-    return Row(
-      mainAxisAlignment:
-          belongsToCurretuser ? MainAxisAlignment.end : MainAxisAlignment.start,
+    return Stack(
       children: [
-        Container(
-          decoration: BoxDecoration(
-            color: belongsToCurretuser
-                ? Colors.grey.shade300
-                : Theme.of(context).primaryColor,
-            borderRadius: const BorderRadius.all(
-              Radius.circular(12),
+        Row(
+          mainAxisAlignment: belongsToCurretuser
+              ? MainAxisAlignment.end
+              : MainAxisAlignment.start,
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                color: belongsToCurretuser
+                    ? Colors.grey.shade300
+                    : Theme.of(context).primaryColor,
+                borderRadius: BorderRadius.only(
+                  topLeft: const Radius.circular(12),
+                  topRight: const Radius.circular(12),
+                  bottomLeft: belongsToCurretuser
+                      ? const Radius.circular(12)
+                      : const Radius.circular(0),
+                  bottomRight: belongsToCurretuser
+                      ? const Radius.circular(0)
+                      : const Radius.circular(12),
+                ),
+              ),
+              width: size / 2.3,
+              padding: const EdgeInsets.symmetric(
+                vertical: 10,
+                horizontal: 16,
+              ),
+              margin: const EdgeInsets.symmetric(
+                vertical: 15,
+              ),
+              child: Column(
+                children: [
+                  Text(
+                    message.username,
+                    style: TextStyle(
+                      color: belongsToCurretuser ? Colors.black : Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    message.text,
+                    style: TextStyle(
+                      color: belongsToCurretuser ? Colors.black : Colors.white,
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ),
-          width: size / 2.3,
-          padding: const EdgeInsets.symmetric(
-            vertical: 10,
-            horizontal: 16,
-          ),
-          child: Column(
-            children: [
-              Text(
-                message.username,
-                style: TextStyle(
-                  color: belongsToCurretuser ? Colors.black : Colors.white,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              Text(
-                message.text,
-                style: TextStyle(
-                  color: belongsToCurretuser ? Colors.black : Colors.white,
-                ),
-              ),
-            ],
-          ),
+          ],
+        ),
+        Positioned(
+          top: 0,
+          left: belongsToCurretuser ? null : 165,
+          right: belongsToCurretuser ? 165 : null,
+          child: _showUserImage(message.userImageUrl),
         ),
       ],
     );

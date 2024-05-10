4b4727f793f13b47f37c35305523a059dafd4a7c
Arnaldo Junior <abs.junnior@hotmail.com>
2024-05-09 21:48:07 -0300

    Configurando faribase no Flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
 import 'package:chate_ease/pages/auth_or_app_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import 'core/services/notification/chat_notification_service.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
